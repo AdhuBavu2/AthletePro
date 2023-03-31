99bbec1805ea8356ac38e6140f0c2055dd89b0da
AdhuBavu2 <adhu.ramki@gmail.com>
2023-03-30 19:34:10 -0600

    admin ui under process

--- src/Admininstrator/Panels/AdminDashboardPanel.java
+++ src/Admininstrator/Panels/AdminDashboardPanel.java
@@ -1,9 +1,26 @@
 package Admininstrator.Panels;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AdminDashboardPanel extends JPanel {
-    public AdminDashboardPanel() {
+    private JLabel announcementLabel;
+    private JLabel dateLabel;
 
+    public AdminDashboardPanel() {
+        setBackground(new Color(0, 100, 246));
+        setLayout(null);
+
+        announcementLabel = new JLabel("Send Announcements");
+        announcementLabel.setBounds(25, 30, 350, 100);
+        announcementLabel.setFont(new Font("Open Sans", Font.BOLD, 24));
+        announcementLabel.setForeground(Color.BLACK);
+        add(announcementLabel);
+
+        dateLabel = new JLabel("Select a date:");
+        dateLabel.setBounds(25, 130, 150, 30);
+        dateLabel.setFont(new Font("Open Sans", Font.PLAIN, 14));
+        dateLabel.setForeground(Color.BLACK);
+        add(dateLabel);
     }
 }
